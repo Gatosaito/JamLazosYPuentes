33062951ea0671cd3fc1e37c0850557e752a3603
Julian2022DE <104095090+Julian2022DE@users.noreply.github.com>
2023-11-19 21:33:39 -0500

    Isla inicial

---
+++ GameJamTGYJ2023/Assets/Scripts/Islas.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Islas : MonoBehaviour
+{
+
+    #region variables
+    [SerializeField] private float frequency;
+    [SerializeField] private float magnitude;
+    [SerializeField] private Vector3 offset_Y;
+    private Vector3 desirePosition;
+    #endregion
+
+    private void Start()
+    {
+        desirePosition = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        transform.localPosition = (desirePosition + transform.up * (Mathf.Sin(Time.time * frequency) * magnitude) + offset_Y);
+    }
+
+}
